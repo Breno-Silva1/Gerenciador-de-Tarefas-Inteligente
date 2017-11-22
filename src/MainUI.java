2fd949b56c58a66b6327a62a65b21e3a229bf3fb
Breno-Silva1 <bbreno100@live.com>
2017-11-22 20:37:19 -0300

    Commitando pelo eclipse direto para o repositório.

--- src/MainUI.java
+++ src/MainUI.java
@@ -4,6 +4,6 @@ public class MainUI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("olá mundo!");
+		System.out.println("Testando commit e push pelo eclipse!");
 	}
-
 }
