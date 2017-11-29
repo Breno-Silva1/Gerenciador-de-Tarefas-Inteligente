17656d917bfe3178661bd847ab81f1a6ced0eb96
Breno-Silva1 <bbreno100@live.com>
2017-11-29 01:35:46 -0300

    Criação da classe controladora Agenda, sem implementação.

---
+++ src/Controller/Agenda.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import Model.Tarefa;
+
+public class Agenda {
+	private Tarefa tarefas[];
+	
+	public void cadastrarTarefa(){
+		
+	}
+	
+	public void removerTarefa(){
+		
+	}
+	
+	public void editarTarefa(){
+		
+	}
+	
+	public void concluirTarefa(){
+		
+	}
+	
+	//Exibir as tarefas
+	
+	public Tarefa[] exibirTodasTarefas(){
+		return tarefas;
+	}
+	
+	public Tarefa[] exibirTarefasDiarias(){
+		return tarefas;
+	}
+	
+	public Tarefa[] exibirTarefasNaoCumpridas(){
+		return tarefas;
+	}
+	
+	public Tarefa[] exibirTarefasConcluidas(){
+		return tarefas;
+	}
+	
+}
