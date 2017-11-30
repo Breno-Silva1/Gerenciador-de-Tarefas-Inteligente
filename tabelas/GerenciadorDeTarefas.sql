0e6f064a844d85dea366742969ceb2b49373caf3
Breno-Silva1 <bbreno100@live.com>
2017-11-29 21:29:10 -0300

    Tabelas do banco de dados e selects.

---
+++ tabelas/GerenciadorDeTarefas.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS GerenciadorDeTarefas;
+CREATE DATABASE GerenciadorDeTarefas DEFAULT CHARACTER SET UTF8;
+
+USE GerenciadorDeTarefas;
+
+CREATE TABLE tarefa (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	titulo VARCHAR(25) NOT NULL,
+	descricao VARCHAR(250),
+	tags VARCHAR(200),
+	prioridade INT(1),
+	data_tarefa VARCHAR(10),
+	estado BOOLEAN
+);
+
+CREATE TABLE meta (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	titulo VARCHAR(25) NOT NULL,
+	descricao VARCHAR(250),
+	tags VARCHAR(200),
+	prioridade INT(1),
+	estado BOOLEAN
+
+);
+
+CREATE TABLE usuario (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200),
+    username VARCHAR(25),
+    email VARCHAR(25),
+    senha VARCHAR(25)
+);
---
+++ tabelas/GerenciadorDeTarefas_Selects.sql
@@ -0,0 +1,4 @@
+USE GerenciadorDeTarefas;
+
+SELECT * FROM tarefa;
+
