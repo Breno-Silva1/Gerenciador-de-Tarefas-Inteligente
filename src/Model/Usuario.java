c1befc6e11b734809538e0296a67ff4da5cb905c
Breno-Silva1 <bbreno100@live.com>
2017-12-01 20:32:35 -0300

    Getters e Setters do atributo id.

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -6,47 +6,56 @@ public class Usuario {
 	private String username;
 	private String email;
 	private String senha;
-	
-	
-	public Usuario(int id, String nome, String username, String email, String senha) {
+
+	public Usuario(String nome, String username, String email, String senha) {
 		super();
-		this.id = id;
 		this.nome = nome;
 		this.username = username;
 		this.email = email;
 		this.senha = senha;
 	}
-	
-	
+
 	public Usuario() {
 		super();
 	}
+	
+	public int getId() {
+		return id;
+	}
 
-
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	
-	
 }
