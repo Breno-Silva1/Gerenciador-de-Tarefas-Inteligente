c45cf7af48a523970ea899b9427ff84e79a07f8a
Breno-Silva1 <bbreno100@live.com>
2017-12-01 20:09:13 -0300

    Adicionando chave estrangeira usuario_id na tabela de tarefas.

--- tabelas/GerenciadorDeTarefas.sql
+++ tabelas/GerenciadorDeTarefas.sql
@@ -3,14 +3,12 @@ CREATE DATABASE GerenciadorDeTarefas DEFAULT CHARACTER SET UTF8;
 
 USE GerenciadorDeTarefas;
 
-CREATE TABLE tarefa (
+CREATE TABLE usuario (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	titulo VARCHAR(25) NOT NULL,
-	descricao VARCHAR(250),
-	tags VARCHAR(200),
-	prioridade INT(1),
-	data_tarefa VARCHAR(10),
-	estado BOOLEAN
+    nome VARCHAR(200),
+    username VARCHAR(25),
+    email VARCHAR(25),
+    senha VARCHAR(25)
 );
 
 CREATE TABLE meta (
@@ -23,10 +21,14 @@ CREATE TABLE meta (
 
 );
 
-CREATE TABLE usuario (
+CREATE TABLE tarefa (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(200),
-    username VARCHAR(25),
-    email VARCHAR(25),
-    senha VARCHAR(25)
+	titulo VARCHAR(25) NOT NULL,
+	descricao VARCHAR(250),
+	tags VARCHAR(200),
+	prioridade INT(1),
+	data_tarefa VARCHAR(10),
+	estado BOOLEAN,
+    id_usuario INT,
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
