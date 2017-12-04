9051f1213f5caac37fe3a802f2e4ce2fed58ef60
Gésio de Moura <gesio.m@academico.ifrn.edu.br>
2017-12-04 01:27:16 -0300

    Metodo Autenticar adicionado em UsuarioDAO

--- src/Controller/UsuarioDAO.java
+++ src/Controller/UsuarioDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import Model.Tarefa;
 import Model.Usuario;
 
 public class UsuarioDAO {
@@ -38,23 +37,21 @@ public class UsuarioDAO {
 	
 	public boolean autenticar(Usuario user){
 		boolean retorno = false;
-		String insert_tarefa = "INSERT INTO usuario (nome, username, email, senha) VALUES (?, ?, ?, ?);";
+		String login = user.getUsername();
+		String senha = user.getSenha();
+		String pesquisarLogin = "SELECT username FROM usuario WHERE username = "+"\""+ login +"\";";
+		String pesquisarSenha = "SELECT senha FROM usuario WHERE senha = "+"\""+ senha +"\";";
 		try {
 			Connection conexao = FabricaDeConexao.getConnection();
-			PreparedStatement stmt = conexao.prepareStatement(insert_tarefa);
-
-			stmt.setString(1, user.getNome());
-			stmt.setString(2, user.getUsername());
-			stmt.setString(3, user.getEmail());
-			stmt.setString(4, user.getSenha());
-
-			if (stmt.executeUpdate() > 0) {
+			PreparedStatement verLogin = conexao.prepareStatement(pesquisarLogin);
+			PreparedStatement verSenha = conexao.prepareStatement(pesquisarSenha);
+			if (verLogin != null && verSenha != null) {
 				retorno = true;
 			} else {
 				retorno = false;
 			}
-			stmt.close();
-			conexao.close();
+			verLogin.close();
+			verSenha.close();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
